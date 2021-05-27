c475cf4d5d24d4837f6c9b784078ff800ecdbf45
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-05-27 20:13:58 +0530

    4, LinkedList

--- Set2/A10/4.cpp
+++ Set2/A10/4.cpp
@@ -1,19 +1,44 @@
+#include <iostream>
+using namespace std;
 template<class T>
 class LinkedList{
-	int data; // Not usuable, just an extra field
-	LinkedList *next;
 public:
+	LinkedList *next;
+	int data;
 	T pop(){
 		LinkedList ret = *next;
 		delete next;
 		return this;
 	}
-	void insert(T t){
-		*next = t;
+	void insert(LinkedList* t){
+		next = t;
 	}
-	T Next(){
-		return *next;
+	LinkedList* Next(){
+		return next;
 	}
 };
 
-int main(){}
+void display(LinkedList<int> * obj){
+	do{
+		cout<<obj->data<<endl;
+	}
+	while((obj=obj->Next())!=NULL);
+}
+
+int main(){
+	int a = 10;
+	int b = 20;
+	int c = 40;
+	
+	LinkedList<int>* a1 = new LinkedList<int>();
+	a1->data = a;
+	LinkedList<int>* b1 = new LinkedList<int>();
+	b1->data = b;
+	LinkedList<int>* c1 = new LinkedList<int>();
+	c1->data = c;
+	
+	b1->insert(c1);
+	a1->insert(b1);
+	
+	display(a1);
+}
